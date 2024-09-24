6ab53e0438afc32ab6a4ff825630e5334cdb76ff
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2024-09-24 10:18:02 +0700

    feat: Add model Category & ProductItem

---
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/ProductItem.cs
@@ -0,0 +1,16 @@
+﻿using HandmadeProductManagement.Core.Base;
+
+namespace HandmadeProductManagement.Contract.Repositories.Entity
+{
+    public class ProductItem : BaseEntity
+    {
+        // Foreign Key Property
+        public string ProductId { get; set; } = string.Empty;
+        //public Product Product { get; set; } = new Product();
+
+        public int QuantityInStock { get; set; }
+        public int Price { get; set; }
+
+
+    }
+}
