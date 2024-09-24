1fc077b5456e12a6e8844b2187c95b056a244b90
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2024-09-24 10:11:42 +0700

    fix: Model from Luan
    - OrderDetail
    - Promotion

--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/OrderDetail.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/OrderDetail.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using HandmadeProductManagement.Core.Base;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HandmadeProductManagement.Contract.Repositories.Entity
 {
-    internal class OrderDetail
+    public class OrderDetail : BaseEntity
     {
+        [MaxLength(50)]
+        public required string OrderId { get; set; }
+
+        [MaxLength(50)]
+        public required string ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public float ProductPrice { get; set; }
+
+
+        // Navigation properties
+        //public virtual Order? Order { get; set; }
+        //public virtual Product? Product { get; set; }
+        //public virtual CancelReason?
     }
 }
--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/Promotion.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/Promotion.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using HandmadeProductManagement.Core.Base;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HandmadeProductManagement.Contract.Repositories.Entity
 {
-    internal class Promotion
+    public class Promotion : BaseEntity
     {
+        [MaxLength(50)]
+        public required string CategoryId { get; set; }
+        [MaxLength(255)]
+        public required string PromotionName { get; set; }
+        public float DiscountRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        [MaxLength(255)]
+        public string? Status { get; set; }
+
     }
 }
