c80ff41627059e623ced2b99f96687135bfeb9cf
Nguyen Son <kijtei1@gmail.com>
2024-09-24 10:31:01 +0700

    feat: add entity CancelReason and StatusChange

--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/StatusChange.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/StatusChange.cs
@@ -12,8 +12,8 @@ namespace HandmadeProductManagement.Contract.Repositories.Entity
     {
         public required DateTime ChangeTime { get; set; }
         public required String Status { get; set; }
-        public required string OrderId { get; set; }
 
+        public required string OrderId { get; set; }
         // public Order Order { get; set; }
     }
 }
