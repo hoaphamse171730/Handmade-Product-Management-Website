88f42cabe62fd419be8be270b868d1b9b2e9c0be
ngckinhluan <nkluantran2907@gmail.com>
2024-09-25 16:18:08 +0700

    feat: update some codes

--- HandmadeProductManagementBE/HandmadeProductManagementBE/obj/Debug/net8.0/HandmadeProductManagementAPI.AssemblyInfo.cs
+++ HandmadeProductManagementBE/HandmadeProductManagementBE/obj/Debug/net8.0/HandmadeProductManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandmadeProductManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dea8731fe938926277211b38ab607c318c8bfa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507339254dec981e8bf96c901ae69cabed60bf08")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandmadeProductManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandmadeProductManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
