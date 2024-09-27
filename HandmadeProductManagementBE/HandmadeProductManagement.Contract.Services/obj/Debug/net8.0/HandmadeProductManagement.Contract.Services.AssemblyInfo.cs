ffa04d1c9305e3fa1164ad51e7ad13eb7161c633
Nguyen Son <kijtei1@gmail.com>
2024-09-27 09:04:24 +0700

    feat: pull code from dev branch

--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Services/obj/Debug/net8.0/HandmadeProductManagement.Contract.Services.AssemblyInfo.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Services/obj/Debug/net8.0/HandmadeProductManagement.Contract.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandmadeProductManagement.Contract.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcacee4defd29c1fc7205746f561aa186b03ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78b5a4e8e3e4c52f4e2336d38684578de83635a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandmadeProductManagement.Contract.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandmadeProductManagement.Contract.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
