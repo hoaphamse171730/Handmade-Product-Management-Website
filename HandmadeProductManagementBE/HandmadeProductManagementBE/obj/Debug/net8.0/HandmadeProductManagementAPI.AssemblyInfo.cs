e622c178d76c6552da8980248795517a1b6fdf15
Nguyen Son <kijtei1@gmail.com>
2024-09-30 11:26:06 +0700

    fix: pull code from dev branch

--- HandmadeProductManagementBE/HandmadeProductManagementBE/obj/Debug/net8.0/HandmadeProductManagementAPI.AssemblyInfo.cs
+++ HandmadeProductManagementBE/HandmadeProductManagementBE/obj/Debug/net8.0/HandmadeProductManagementAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandmadeProductManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730c4092ef4abe21c005eb3d1642478f46b90076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36be684a0503fc434031cbcd86ea41dd6dbfa121")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandmadeProductManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandmadeProductManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
