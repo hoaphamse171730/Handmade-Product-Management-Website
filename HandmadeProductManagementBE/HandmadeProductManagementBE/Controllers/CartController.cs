9691108d88725222f9419254f1c01baeea5df456
fishmakeweb <duongviethoang.1909@gmail.com>
2024-10-01 16:17:52 +0700

    fix : update response message

--- HandmadeProductManagementBE/HandmadeProductManagementBE/Controllers/CartController.cs
+++ HandmadeProductManagementBE/HandmadeProductManagementBE/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using HandmadeProductManagement.Core.Constants;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using HandmadeProductManagement.Contract.Services;
+using HandmadeProductManagement.Contract.Repositories.Entity;
 
 namespace HandmadeProductManagementAPI.Controllers
 {
@@ -24,136 +25,55 @@ namespace HandmadeProductManagementAPI.Controllers
         [HttpGet("{userId}")]
         public async Task<IActionResult> GetCart([Required] Guid userId)
         {
-            try
+            var cart = await _cartService.GetCartByUserId(userId);
+            var response = new BaseResponse<CartModel>
             {
-                var cart = await _cartService.GetCartByUserId(userId);
-                var response = new BaseResponse<CartModel>
-                {
-                    Code = "Success",
-                    StatusCode = StatusCodeHelper.OK,
-                    Message = "Cart retrieved successfully.",
-                    Data = cart
-                };
-                return Ok(response);
-            }
-            catch (ArgumentException ex)
-            {
-                var response = new BaseResponse<CartModel>
-                {
-                    Code = "NotFound",
-                    StatusCode = StatusCodeHelper.NotFound,
-                    Message = ex.Message,
-                    Data = null
-                };
-                return NotFound(response);
-            }
-            catch (Exception)
-            {
-                var response = new BaseResponse<CartModel>
-                {
-                    Code = "ServerError",
-                    StatusCode = StatusCodeHelper.ServerError,
-                    Message = "An unexpected error occurred.",
-                    Data = null
-                };
-                return StatusCode(500, response);
-            }
+                Code = "Success",
+                StatusCode = StatusCodeHelper.OK,
+                Message = "Cart retrieved successfully.",
+                Data = cart
+            };
+            return Ok(response);
         }
 
         [HttpPost("item/add/{cartId}")]
         public async Task<IActionResult> AddCartItem([Required] string cartId, [FromBody] CreateCartItemDto createCartItemDto)
         {
-            try
+            var response = new BaseResponse<bool>
             {
-                var response = await _cartItemService.AddCartItem(cartId, createCartItemDto);
-                return Ok(response);
-            }
-            catch (ArgumentException ex)
-            {
-                var response = new BaseResponse<bool>
-                {
-                    Code = "BadRequest",
-                    StatusCode = StatusCodeHelper.BadRequest,
-                    Message = ex.Message,
-                    Data = false
-                };
-                return BadRequest(response);
-            }
-            catch (Exception)
-            {
-                var response = new BaseResponse<bool>
-                {
-                    Code = "ServerError",
-                    StatusCode = StatusCodeHelper.ServerError,
-                    Message = "An unexpected error occurred.",
-                    Data = false
-                };
-                return StatusCode(500, response);
-            }
+                Code = "Success",
+                StatusCode = StatusCodeHelper.OK,
+                Message = "Add Cart Item successfully.",
+                Data = await _cartItemService.AddCartItem(cartId, createCartItemDto);
+            };
+            return Ok(response);
         }
 
         [HttpPut("item/updateQuantity/{cartItemId}")]
         public async Task<IActionResult> UpdateCartItem([Required] string cartItemId, [Required] int productQuantity)
         {
-            try
+
+            var response = new BaseResponse<bool>
             {
-                var response = await _cartItemService.UpdateCartItem(cartItemId, productQuantity);
-                return Ok(response);
-            }
-            catch (ArgumentException ex)
-            {
-                var response = new BaseResponse<bool>
-                {
-                    Code = "BadRequest",
-                    StatusCode = StatusCodeHelper.BadRequest,
-                    Message = ex.Message,
-                    Data = false
-                };
-                return BadRequest(response);
-            }
-            catch (Exception)
-            {
-                var response = new BaseResponse<bool>
-                {
-                    Code = "ServerError",
-                    StatusCode = StatusCodeHelper.ServerError,
-                    Message = "An unexpected error occurred.",
-                    Data = false
-                };
-                return StatusCode(500, response);
-            }
+                Code = "Success",
+                StatusCode = StatusCodeHelper.OK,
+                Message = "Update Cart Item successfully.",
+                Data = await _cartItemService.UpdateCartItem(cartItemId, productQuantity);
+            };
+            return Ok(response);
         }
 
         [HttpDelete("item/{cartItemId}")]
         public async Task<IActionResult> RemoveCartItem([Required] string cartItemId)
         {
-            try
+            var response = new BaseResponse<bool>
             {
-                var response = await _cartItemService.RemoveCartItem(cartItemId);
-                return Ok(response);
-            }
-            catch (ArgumentException ex)
-            {
-                var response = new BaseResponse<bool>
-                {
-                    Code = "BadRequest",
-                    StatusCode = StatusCodeHelper.BadRequest,
-                    Message = ex.Message,
-                    Data = false
-                };
-                return BadRequest(response);
-            }
-            catch (Exception)
-            {
-                var response = new BaseResponse<bool>
-                {
-                    Code = "ServerError",
-                    StatusCode = StatusCodeHelper.ServerError,
-                    Message = "An unexpected error occurred.",
-                    Data = false
-                };
-                return StatusCode(500, response);
-            }
+                Code = "Success",
+                StatusCode = StatusCodeHelper.OK,
+                Message = "Remove Cart Item successfully.",
+                Data = await _cartItemService.RemoveCartItem(cartItemId);
+            };
+            return Ok(response);
         }
     }
 }
