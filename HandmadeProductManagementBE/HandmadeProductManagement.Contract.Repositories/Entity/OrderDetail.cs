55ff4f490c48224aa452aa952e979f68e1d84ad4
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2024-10-04 13:58:30 +0700

    fix: Delete DiscountPrice/Rate in ProductItem and Add in OrderDetail

--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/OrderDetail.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/OrderDetail.cs
@@ -13,9 +13,8 @@ namespace HandmadeProductManagement.Contract.Repositories.Entity
         public string ProductItemId { get; set; }
         public string OrderId { get; set; }
         public int ProductQuantity { get; set; }
-        public decimal UnitPrice { get; set; }
+        public decimal DiscountPrice { get; set; }
 
-        
         public virtual Order? Order { get; set; }
         public virtual ProductItem? ProductItem { get; set; }
       
