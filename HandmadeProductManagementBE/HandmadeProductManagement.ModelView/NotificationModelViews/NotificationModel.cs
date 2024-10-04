b9862fa9339157e851e2b1c67b8a3cb03c6ed274
ngckinhluan <nkluantran2907@gmail.com>
2024-10-04 13:29:00 +0700

    merge

---
+++ HandmadeProductManagementBE/HandmadeProductManagement.ModelView/NotificationModelViews/NotificationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandmadeProductManagement.ModelViews.NotificationModelViews
+{
+    public class NotificationModel
+    {
+        public string Id { get; set; }
+        public string? Message { get; set; }
+
+        public string? Tag { get; set; }
+        public string? URL { get; set; }
+    }
+}
