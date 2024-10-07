88459c1ad650bb54717be56d10f9d2df13423e90
Nguyen Son <kijtei1@gmail.com>
2024-10-07 14:55:23 +0700

    feat: add authorization

--- HandmadeProductManagementBE/HandmadeProductManagement.Core/Exceptions/Handler/CustomExceptionHandler.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Core/Exceptions/Handler/CustomExceptionHandler.cs
@@ -5,71 +5,70 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace HandmadeProductManagement.Core.Exceptions.Handler
+namespace HandmadeProductManagement.Core.Exceptions.Handler;
+
+public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
 {
-    public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
     {
-        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        logger.LogError("Error Message: {exceptionMessage}, Time of occurrence: {time}",
+            exception.Message, DateTime.UtcNow);
+
+        (string Detail, string Title, int StatusCode) details = exception switch
         {
-            logger.LogError("Error Message: {exceptionMessage}, Time of occurrence: {time}",
-                exception.Message, DateTime.UtcNow);
+            ValidationException validationException => (
+                validationException.Message,
+                validationException.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
 
-            (string Detail, string Title, int StatusCode) details = exception switch
-            {
-                ValidationException validationException => (
-                    validationException.Message,
-                    validationException.GetType().Name,
-                    context.Response.StatusCode = StatusCodes.Status400BadRequest
-                ),
+            BaseException.BadRequestException badRequestException => (
+                badRequestException.ErrorDetail.ErrorMessage?.ToString() ?? exception.Message,
+                badRequestException.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
 
-                BaseException.BadRequestException badRequestException => (
-                    badRequestException.ErrorDetail.ErrorMessage?.ToString() ?? exception.Message,
-                    badRequestException.GetType().Name,
-                    context.Response.StatusCode = StatusCodes.Status400BadRequest
-                ),
+            BaseException.NotFoundException notFoundException => (
+                notFoundException.ErrorDetail.ErrorMessage?.ToString() ?? exception.Message,
+                notFoundException.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status404NotFound
+            ),
 
-                BaseException.NotFoundException notFoundException => (
-                    notFoundException.ErrorDetail.ErrorMessage?.ToString() ?? exception.Message,
-                    notFoundException.GetType().Name,
-                    context.Response.StatusCode = StatusCodes.Status404NotFound
-                ),
-
-                UnauthorizedAccessException unauthorizedAccessException => (
+            UnauthorizedAccessException unauthorizedAccessException => (
                     "Access is denied due to invalid credentials.",
                     unauthorizedAccessException.GetType().Name,
                     context.Response.StatusCode = StatusCodes.Status401Unauthorized
                 ),
 
-                BaseException.ForbiddenException forbiddenException => (
-                    forbiddenException.ErrorDetail.ErrorMessage?.ToString() ?? "You do not have permission to perform this action.",
-                    forbiddenException.GetType().Name,
-                    context.Response.StatusCode = StatusCodes.Status403Forbidden
-                ),
+            BaseException.ForbiddenException forbiddenException => (
+                forbiddenException.ErrorDetail.ErrorMessage?.ToString() ?? "You do not have permission to perform this action.",
+                forbiddenException.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status403Forbidden
+            ),
 
-                _ => (
-                    exception.Message,
-                    exception.GetType().Name,
-                    context.Response.StatusCode = StatusCodes.Status500InternalServerError
-                )
-            };
+            _ => (
+                exception.Message,
+                exception.GetType().Name,
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            )
+        };
 
-            var problemDetails = new ProblemDetails()
-            {
-                Title = details.Title,
-                Detail = details.Detail,
-                Status = details.StatusCode,
-                Instance = context.Request.Path
-            };
+        var problemDetails = new ProblemDetails()
+        {
+            Title = details.Title,
+            Detail = details.Detail, // Here we pass the correct custom error message
+            Status = details.StatusCode,
+            Instance = context.Request.Path
+        };
 
-            problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+        problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
 
-            if (exception is ValidationException vEx)
-            {
-                problemDetails.Extensions.Add("ValidationErrors", vEx.Errors);
-            }
-
-            await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
-            return true;
+        if (exception is ValidationException vEx)
+        {
+            problemDetails.Extensions.Add("ValidationErrors", vEx.Errors);
         }
+
+        await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+        return true;
     }
 }
