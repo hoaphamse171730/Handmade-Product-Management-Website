d837f04e591e9f782a5cc66b5cec2f013f8fb155
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2024-10-08 08:58:50 +0700

    fix: payment detail + paymentService

--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Services/Interface/IPaymentService.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Services/Interface/IPaymentService.cs
@@ -9,7 +9,7 @@ namespace HandmadeProductManagement.Contract.Services.Interface
 {
     public interface IPaymentService
     {
-        Task<bool> CreatePaymentAsync(string userId, CreatePaymentDto createPaymentDto);
+        Task<bool> CreatePaymentAsync(string userId, string orderId);
         Task<bool> UpdatePaymentStatusAsync(string paymentId, string status, string userId);
         Task<PaymentResponseModel> GetPaymentByOrderIdAsync(string orderId);
         Task CheckAndExpirePaymentsAsync();
