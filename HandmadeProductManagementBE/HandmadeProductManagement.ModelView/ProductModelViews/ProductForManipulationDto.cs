1cbb9b0aac4a20cd42f5cf3c0341ddea0e43d7c0
ngckinhluan <nkluantran2907@gmail.com>
2024-10-08 10:39:42 +0700

    feat: add unit tests for Product and OrderDetail

--- HandmadeProductManagementBE/HandmadeProductManagement.ModelView/ProductModelViews/ProductForManipulationDto.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.ModelView/ProductModelViews/ProductForManipulationDto.cs
@@ -11,9 +11,5 @@ namespace HandmadeProductManagement.ModelViews.ProductModelViews
         public string? Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? CategoryId { get; set; } = string.Empty;
-        public string? ShopId { get; set; } = string.Empty;
-        public decimal? Rating { get; set; }
-        public string? Status { get; set; } = string.Empty;
-        public int? SoldCount { get; set; }
     }
 }
--- HandmadeProductManagementBE/HandmadeProductManagement.ModelView/PromotionModelViews/PromotionForManipulationDto.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.ModelView/PromotionModelViews/PromotionForManipulationDto.cs
@@ -9,5 +9,4 @@ public class PromotionForManipulationDto
     public decimal? DiscountRate { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
-    public string? Status { get; set; }
 }
