33c5e27af6d1fe64530d9704582e98be222ff89c
Nguyen Son <kijtei1@gmail.com>
2024-10-09 16:28:13 +0700

    fix: delete unecessary files

--- HandmadeProductManagementBE/HandmadeProductManagement.ModelView/VariationModelViews/VariationWithOptionDto.cs
+++
@@ -1,8 +0,0 @@
-﻿using HandmadeProductManagement.ModelViews.VariationOptionModelViews;
-
-public class VariationWithOptionsDto
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public IList<VariationOptionDto> Options { get; set; }
-}
