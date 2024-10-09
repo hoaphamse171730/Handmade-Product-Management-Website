4cbcff513c5b5dca0ecae38ba57dbca6d039270e
Nguyen Minh Tuan (k18 HCM) <TuanNMSE184386@fpt.edu.vn>
2024-10-09 14:08:33 +0700

    fix typo

--- HandmadeProductManagementBE/HandmadeProductManagementBE/Controllers/DashboardController.cs
+++ HandmadeProductManagementBE/HandmadeProductManagementBE/Controllers/DashboardController.cs
@@ -25,7 +25,7 @@ using Microsoft.AspNetCore.Mvc;
             }
 
         [HttpGet("total-orders")]
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult>GetTotalOrders()
         {
             int totalOrders = await _dashboardService.GetTotalOrders();
@@ -34,7 +34,7 @@ using Microsoft.AspNetCore.Mvc;
             }
 
         [HttpGet("total-products")]
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetTotalProducts()
         {
             int totalProduct = await _dashboardService.GetTotalProducts();
@@ -43,7 +43,7 @@ using Microsoft.AspNetCore.Mvc;
         }
 
         [HttpGet("total-sales")]
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetTotalSales()
         {
             decimal totalSales = await _dashboardService.GetTotalSales();
@@ -52,7 +52,7 @@ using Microsoft.AspNetCore.Mvc;
         }
 
         [HttpGet("top10-shops")]
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetTop10Shops()
         {
            List<Shop> topShops = await _dashboardService.GetTop10Shops();
@@ -61,7 +61,7 @@ using Microsoft.AspNetCore.Mvc;
 
         }
         [HttpPost("TotalSaleById")]
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> TotalSaleByShopId(string Id, DashboardDTO dashboardDTO)
         {
             decimal totalSale = await _dashboardService.GetTotalSaleByShopId(Id, dashboardDTO);
@@ -69,7 +69,7 @@ using Microsoft.AspNetCore.Mvc;
             return Ok(BaseResponse<decimal>.OkResponse(totalSale)); 
         }
         [HttpPost("TopSellingProducts")]
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> TopSellingProducts()
         {
             List<Product> topSellingProducts = await _dashboardService.GetTopSellingProducts();
@@ -77,6 +77,8 @@ using Microsoft.AspNetCore.Mvc;
         }
 
         [HttpGet("GetTop10NewProduct")]
+        [Authorize(Roles = "Admin")]
+
         public async Task<IActionResult> Top10NewProducts()
         {
             var products = await _dashboardService.GetTop10NewProducts();
