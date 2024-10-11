ee8f8a84d8bb41778a1ab12a10c493ccb586ca18
annt1303 <anngtr1303@gmail.com>
2024-10-11 15:50:32 +0700

    Fix: fix function get order by page

--- HandmadeProductManagementBE/HandmadeProductManagement.Services/Service/OrderService.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Services/Service/OrderService.cs
@@ -148,7 +148,7 @@ namespace HandmadeProductManagement.Services.Service
                         };
 
                         await _orderDetailService.Create(orderDetail);
-                        await _cartItemService.DeleteCartItemByIdAsync(cartItem.Id);
+                        //await _cartItemService.DeleteCartItemByIdAsync(cartItem.Id);
                     }
 
                     await _unitOfWork.SaveAsync();
@@ -177,6 +177,8 @@ namespace HandmadeProductManagement.Services.Service
         public async Task<PaginatedList<OrderResponseModel>> GetOrdersByPageAsync(int pageNumber, int pageSize)
         {
             var repository = _unitOfWork.GetRepository<Order>();
+            var orderDetailRepository = _unitOfWork.GetRepository<OrderDetail>();
+
             var query = repository.Entities.Where(order => !order.DeletedTime.HasValue);
 
             var totalItems = await query.CountAsync();
@@ -195,6 +197,16 @@ namespace HandmadeProductManagement.Services.Service
                     Phone = order.Phone,
                     Note = order.Note,
                     CancelReasonId = order.CancelReasonId,
+                    OrderDetails = orderDetailRepository.Entities
+                        .Where(od => od.OrderId == order.Id && !od.DeletedTime.HasValue)
+                        .Select(od => new OrderDetailDto
+                        {
+                            Id = od.Id,
+                            ProductItemId = od.ProductItemId,
+                            OrderId = od.OrderId,
+                            ProductQuantity = od.ProductQuantity,
+                            DiscountPrice = od.DiscountPrice,
+                        }).ToList()
                 })
                 .ToListAsync();
 
