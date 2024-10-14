810affed7a1d347817413f09142726de0120d115
ngckinhluan <nkluantran2907@gmail.com>
2024-10-14 16:24:12 +0700

    Fix: Fix GraphQL

--- HandmadeProductManagementBE/HandmadeProductManagementBE/GraphQL/Mutations/PromotionMutation.cs
+++ HandmadeProductManagementBE/HandmadeProductManagementBE/GraphQL/Mutations/PromotionMutation.cs
@@ -5,18 +5,10 @@ namespace HandmadeProductManagementAPI.GraphQL.Mutations;
 
 public class PromotionMutation
 {
-    public async Task<bool> CreatePromotion([Service] IPromotionService promotionService, PromotionForCreationDto promotion)
-    {
-        return await promotionService.Create(promotion);
-    }
+    public async Task<bool> CreatePromotion([Service] IPromotionService promotionService, PromotionForCreationDto promotion, string userId) => await promotionService.Create(promotion, userId);
 
-    public async Task<bool> UpdatePromotion([Service] IPromotionService promotionService, string id, PromotionForUpdateDto promotion)
-    {
-        return await promotionService.Update(id, promotion);
-    }
-
-    public async Task<bool> SoftDeletePromotion([Service] IPromotionService promotionService, string id)
-    {
-        return await promotionService.SoftDelete(id);
-    }
+    public async Task<bool> UpdatePromotion([Service] IPromotionService promotionService, string id, PromotionForUpdateDto promotion, string userId) => await promotionService.Update(id, promotion, userId);
+    
+    public async Task<bool> SoftDeletePromotion([Service] IPromotionService promotionService, string id) => await promotionService.SoftDelete(id);
+   
 }
