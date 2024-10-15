8dba39634f403faca9e4b5a259f12d975ca724dd
Nguyen Son <kijtei1@gmail.com>
2024-10-15 09:33:09 +0700

    fix: cart quantity

--- HandmadeProductManagementBE/HandmadeProductManagementBE/Controllers/CartItemController.cs
+++ HandmadeProductManagementBE/HandmadeProductManagementBE/Controllers/CartItemController.cs
@@ -19,8 +19,7 @@ namespace HandmadeProductManagement.Controllers
             _cartItemService = cartItemService;
         }
 
-        [Authorize]
-        //[Authorize(Roles = "Customer")]
+        [Authorize(Roles = "Customer")]
         // GET: api/cartitem
         [HttpGet]
         public async Task<IActionResult> GetCartItems()
@@ -36,8 +35,7 @@ namespace HandmadeProductManagement.Controllers
             return Ok(response);
         }
 
-        [Authorize]
-        //[Authorize(Roles = "Customer")]
+        [Authorize(Roles = "Customer")]
         // POST: api/cartitem
         [HttpPost]
         public async Task<IActionResult> AddCartItem(CartItemForCreationDto createCartItemDto)
@@ -66,8 +64,7 @@ namespace HandmadeProductManagement.Controllers
             }
         }
 
-        [Authorize]
-        //[Authorize(Roles = "Customer")]
+        [Authorize(Roles = "Customer")]
         // PUT: api/cartitem/{cartItemId}
         [HttpPut("{cartItemId}")]
         public async Task<IActionResult> UpdateCartItem(string cartItemId, [FromBody] CartItemForUpdateDto updateCartItemDto)
@@ -96,8 +93,7 @@ namespace HandmadeProductManagement.Controllers
             }
         }
 
-        [Authorize]
-        //[Authorize(Roles = "Customer")]
+        [Authorize(Roles = "Customer")]
         // DELETE: api/cartitem/{cartItemId}
         [HttpDelete("{cartItemId}")]
         public async Task<IActionResult> DeleteCartItem(string cartItemId)
@@ -127,8 +123,7 @@ namespace HandmadeProductManagement.Controllers
             
         }
 
-        [Authorize]
-        //[Authorize(Roles = "Customer")]
+        [Authorize(Roles = "Customer")]
         // GET: api/cartitem/total
         [HttpGet("total")]
         public async Task<IActionResult> GetTotalCartPrice()
