ba327515cca00071a514794bc5812f42b9c40d84
Nguyen Minh Tuan (k18 HCM) <TuanNMSE184386@fpt.edu.vn>
2024-10-16 09:31:17 +0700

    fix logic VNPAY

--- HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/Payment.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.Contract.Repositories/Entity/Payment.cs
@@ -13,6 +13,7 @@ namespace HandmadeProductManagement.Contract.Repositories.Entity
         public decimal TotalAmount { get; set; }
         public string Status { get; set; } = string.Empty;
 
+        public string Method {  get; set; } = string.Empty;
         [JsonIgnore]
         public Order? Order { get; set; }
 
