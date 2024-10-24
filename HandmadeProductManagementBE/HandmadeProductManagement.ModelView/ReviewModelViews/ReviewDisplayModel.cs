56b2fdb59f5da5e368f3fd885954ad8a4ea4bca7
phiyenn <155460832+fiyenn@users.noreply.github.com>
2024-10-24 12:05:39 +0700

    fix: Review UI + feat: Reply Razor Page

---
+++ HandmadeProductManagementBE/HandmadeProductManagement.ModelView/ReviewModelViews/ReviewDisplayModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandmadeProductManagement.ModelViews.ReviewModelViews
+{
+    public class ReviewDisplayModel : ReviewModel
+    {
+        public string UserDisplayName { get; set; } = string.Empty;
+        public string ShopName { get; set; } = string.Empty;
+    }
+}
