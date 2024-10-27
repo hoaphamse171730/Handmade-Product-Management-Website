a087afdec7982f5077562b56d359663349c291d5
Nguyen Minh Tuan (k18 HCM) <TuanNMSE184386@fpt.edu.vn>
2024-10-28 00:01:12 +0700

    fix edit user

---
+++ HandmadeProductManagementBE/UI/Pages/User/User.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages.User
+{
+    public class UserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
