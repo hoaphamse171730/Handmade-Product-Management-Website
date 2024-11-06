897f9da6893a38b2fe147a74e2a50e662ba9acb7
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2024-11-06 14:46:25 +0700

    feat: add Manage Shop for Admin

---
+++ HandmadeProductManagementBE/HandmadeProductManagement.ModelView/ShopModelViews/ShopDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandmadeProductManagement.ModelViews.ShopModelViews
+{
+    public class ShopDto
+    {
+        public string Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Rating { get; set; }
+        public decimal TotalSales { get; set; }
+        public string userId { get; set; } = string.Empty;
+        public string ownerName { get; set; } = string.Empty ;
+
+    }
+}
