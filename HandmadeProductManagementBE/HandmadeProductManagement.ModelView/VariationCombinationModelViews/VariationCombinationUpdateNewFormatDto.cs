c9c9ccc68752d89f7eddde44bc6eb80304695d3a
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2024-11-14 11:39:13 +0700

    fix: update dto for productEdit

--- HandmadeProductManagementBE/HandmadeProductManagement.ModelView/VariationCombinationModelViews/VariationCombinationUpdateNewFormatDto.cs
+++ HandmadeProductManagementBE/HandmadeProductManagement.ModelView/VariationCombinationModelViews/VariationCombinationUpdateNewFormatDto.cs
@@ -4,8 +4,7 @@ namespace HandmadeProductManagement.ModelViews.VariationCombinationModelViews
 {
     public class VariationCombinationUpdateNewFormatDto
     {
-        public string ProductItemId { get; set; } = string.Empty;
-        public List<OptionsDto>? Combinations { get; set; }
+        public List<string>? VariationOptionIds { get; set; }
         public int? Price { get; set; }
         public int? QuantityInStock { get; set; }
     }
